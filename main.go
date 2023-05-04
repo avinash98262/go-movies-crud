dbf47c9de9cbd0f34290039c3fbbb126ec77536c
avinash9826 <pandeyavinash960@gmail.com>
2023-05-04 21:01:25 +0530

    fourth

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Movie struct {
@@ -15,19 +17,105 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "welcome to the Golang world")
+var movies []Movie
 
-	details := Movie{
-		Title: "tiger",
-		Id:    "123",
+// func hello(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "welcome to the Golang world")
+
+// 	details := Movie{
+// 		Title: "tiger",
+// 		Id:    "123",
+// 	}
+// 	fmt.Fprintln(w, details.Title)
+// 	fmt.Fprintln(w, details.Id)
+// }
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
 	}
-	fmt.Fprintln(w, details.Title)
-	fmt.Fprintln(w, details.Id)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var movie Movie
+	json.NewDecoder(r.Body).Decode(&movie)
+	// ----->
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	//params
+	// loop over the movies and range
+	for index, item := range movies {
+
+		// delete the movie that  id , whichever you have sent
+
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			//append the new movie
+			var movie Movie
+			json.NewDecoder(r.Body).Decode(&movie)
+			movie.Id = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movies)
+		}
+	}
+
 }
 
 func main() {
 
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	movies = append(movies, Movie{Title: "Dhoom", Id: "234", Director: &Director{Firstname: "karan", Lastname: "johar"}})
+	movies = append(movies, Movie{Title: "Jai ho", Id: "421", Director: &Director{Firstname: "prince", Lastname: "raj"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+
+	// http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", r)
 }
