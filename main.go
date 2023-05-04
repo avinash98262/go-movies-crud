46b7fd8de43a384847cc8d5d0990848fb44279db
avinash9826 <pandeyavinash960@gmail.com>
2023-05-04 12:42:49 +0530

    fourth time

--- main.go
+++ main.go
@@ -5,11 +5,29 @@ import (
 	"net/http"
 )
 
+type Movie struct {
+	Title    string    `json:"title"`
+	Id       string    `json:"id"`
+	Director *Director `json:"director"`
+}
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to the Golang world")
+	fmt.Fprintln(w, "welcome to the Golang world")
+
+	details := Movie{
+		Title: "tiger",
+		Id:    "123",
+	}
+	fmt.Fprintln(w, details.Title)
+	fmt.Fprintln(w, details.Id)
 }
 
 func main() {
+
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8080", nil)
 }
